e1dd9fdb005c668d2e61549cba1f04af093fed1e
Iryna Kachuk <kachuk.irina@mail.ru>
2024-10-18 07:01:57 +0400

    demo_residue

--- src/DrawingSystem/ResidueDrawer.py
+++ src/DrawingSystem/ResidueDrawer.py
@@ -11,6 +11,8 @@ import os
 
 import scipy.stats as stats
 
+import numpy as np
+
 class ResidueDrawer(DrawingSystem):
     def __init__(self) -> None:
         super().__init__()
@@ -33,18 +35,27 @@ class ResidueDrawer(DrawingSystem):
                 #axs[0] = plot(x_true, residue, marker = ... line cmap )
                 scatter = axs[0].scatter(x_true, residue, color = 'blue' , cmap = cmap)
                 axs[0].axhline(y = 0, color = 'red', linestyle = '--')
-                axs[0].set_title('Residuals')
-                axs[0].set_xlabel('True Values')
-                axs[0].set_ylabel('Residuals')
+                axs[0].set_title("График распределения остатков")
+                axs[0].set_xlabel("Истинные значения")
+                axs[0].set_ylabel("Остатки")
 
                 stats.probplot(residue, dist = 'norm', plot = axs[1])
-                axs[1].set_title('Q-Q Plot of the Residuals')
-            
-                #bins???
+                axs[1].set_title("График квантилей для остатков")
+                axs[1].set_xlabel("Наблюдаемые значения")
+                axs[1].set_ylabel("Ожидаемые квантили")
+
                 axs[2].hist(residue, bins = 30, color = 'blue', alpha = 0.7, edgecolor = 'black')
-                axs[2].set_title('Histogram of the Residuals')
-                axs[2].set_xlabel('Residuals')
-                axs[2].set_ylabel('Frequency')
+                               
+                mu, std = np.mean(residue), np.std(residue)
+                xmin, xmax = plt.xlim()
+                x = np.linspace(xmin, xmax, 100)
+                p = stats.norm.pdf(x, mu, std)
+
+                axs[2].plot(x, p, 'r', linewidth=2)
+
+                axs[2].set_title("Гистограмма распределения остатков")
+                axs[2].set_xlabel("Остатки")
+                axs[2].set_ylabel("Относительная частота")
 
             filepath = os.path.join(BASE_OUTPUT_PATH, path)
             plt.savefig(filepath)
@@ -57,19 +68,29 @@ class ResidueDrawer(DrawingSystem):
         fig, axs = plt.subplots(3, 1, figsize = (10, 12))
 
         for x_true, residue, cmap in self.residuals:
-            scatter = axs[0].scatter(x_true, residue, color = 'blue' , cmap = cmap)
-            axs[0].axhline(y = 0, color = 'red', linestyle = '--')
-            axs[0].set_title('Residuals')
-            axs[0].set_xlabel('True Values')
-            axs[0].set_ylabel('Residuals')
+                scatter = axs[0].scatter(x_true, residue, color = 'blue' , cmap = cmap)
+                axs[0].axhline(y = 0, color = 'red', linestyle = '--')
+                axs[0].set_title("График распределения остатков")
+                axs[0].set_xlabel("Истинные значения")
+                axs[0].set_ylabel("Остатки")
 
-            stats.probplot(residue, dist = 'norm', plot = axs[1])
-            axs[1].set_title('Q-Q Plot of the Residuals')
+                stats.probplot(residue, dist = 'norm', plot = axs[1])
+                axs[1].set_title("График квантилей для остатков")
+                axs[1].set_xlabel("Наблюдаемые значения")
+                axs[1].set_ylabel("Ожидаемые квантили")
             
-            axs[2].hist(residue, bins = 30, color = 'blue', alpha = 0.7, edgecolor = 'black')
-            axs[2].set_title('Histogram of the Residuals')
-            axs[2].set_xlabel('Residuals')
-            axs[2].set_ylabel('Frequency')
+                axs[2].hist(residue, bins = 30, color = 'blue', alpha = 0.7, edgecolor = 'black')
+                               
+                mu, std = np.mean(residue), np.std(residue)
+                xmin, xmax = plt.xlim()
+                x = np.linspace(xmin, xmax, 100)
+                p = stats.norm.pdf(x, mu, std)
+
+                axs[2].plot(x, p, 'r', linewidth=2)
+
+                axs[2].set_title("Гистограмма распределения остатков")
+                axs[2].set_xlabel("Остатки")
+                axs[2].set_ylabel("Относительная частота")
 
         plt.tight_layout()
         plt.show()
